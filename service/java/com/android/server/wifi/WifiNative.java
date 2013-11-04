941064d9c635966ff2289d49541c810b6a44cdb7
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2013-11-04 02:54:13 +0000

    wifi: Set the mode at native layer
     * Set normal/AP mode so that the HAL can specify correct module
       arguments when loading the driver.
    
    Change-Id: I6eee2cc53b5f1e70306e252e52c909833f220515

--- service/java/com/android/server/wifi/WifiNative.java
+++ service/java/com/android/server/wifi/WifiNative.java
@@ -1239,6 +1239,8 @@ public class WifiNative {
         return doBooleanCommand("NFC_REPORT_HANDOVER RESP P2P " + requestMessage + " 00");
     }
 
+    public native static boolean setMode(int mode);
+
     /* WIFI HAL support */
 
     private static final String TAG = "WifiNative-HAL";
--- service/java/com/android/server/wifi/WifiStateMachine.java
+++ service/java/com/android/server/wifi/WifiStateMachine.java
@@ -2100,6 +2100,7 @@ public class WifiStateMachine extends StateMachine implements WifiNative.WifiPno
      */
     public void setSupplicantRunning(boolean enable) {
         if (enable) {
+            WifiNative.setMode(0);
             sendMessage(CMD_START_SUPPLICANT);
         } else {
             sendMessage(CMD_STOP_SUPPLICANT);
@@ -2111,6 +2112,7 @@ public class WifiStateMachine extends StateMachine implements WifiNative.WifiPno
      */
     public void setHostApRunning(WifiConfiguration wifiConfig, boolean enable) {
         if (enable) {
+            WifiNative.setMode(1);
             sendMessage(CMD_START_AP, wifiConfig);
         } else {
             sendMessage(CMD_STOP_AP);
