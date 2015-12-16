818a1ae817174fe357ca46e8fe994d325d6cf669
Steve Kondik <steve@cyngn.com>
2015-12-16 13:43:32 -0500

    wifi: Configure additional TCP parameters
     * Improves WiFi display / casting performance
    
    Change-Id: I728ba47be9795c979380160d1b145d8cd9a1e9b4

--- service/java/com/android/server/wifi/WifiStateMachine.java
+++ service/java/com/android/server/wifi/WifiStateMachine.java
@@ -1110,6 +1110,8 @@ public class WifiStateMachine extends StateMachine implements WifiNative.WifiPno
     private final IBatteryStats mBatteryStats;
 
     private String mTcpBufferSizes = null;
+    private int mTcpDelayedAckSegments = 1;
+    private int mTcpUserCfg = 0;
 
     // Used for debug and stats gathering
     private static int sScanAlarmIntentCount = 0;
@@ -1352,6 +1354,8 @@ public class WifiStateMachine extends StateMachine implements WifiNative.WifiPno
 
         mTcpBufferSizes = mContext.getResources().getString(
                 com.android.internal.R.string.config_wifi_tcp_buffers);
+        mTcpDelayedAckSegments = SystemProperties.getInt("net.tcp.delack.wifi", 1);
+        mTcpUserCfg = SystemProperties.getInt("net.tcp.usercfg.wifi", 0);
 
         addState(mDefaultState);
             addState(mInitialState, mDefaultState);
@@ -4752,6 +4756,8 @@ public class WifiStateMachine extends StateMachine implements WifiNative.WifiPno
         if (!TextUtils.isEmpty(mTcpBufferSizes)) {
             newLp.setTcpBufferSizes(mTcpBufferSizes);
         }
+        newLp.setTcpDelayedAckSegments(mTcpDelayedAckSegments);
+        newLp.setTcpUserCfg(mTcpUserCfg);
 
         // IPv4/v6 addresses, IPv6 routes and IPv6 DNS servers come from netlink.
         LinkProperties netlinkLinkProperties = mNetlinkTracker.getLinkProperties();
@@ -8479,6 +8485,9 @@ public class WifiStateMachine extends StateMachine implements WifiNative.WifiPno
             if (!TextUtils.isEmpty(mTcpBufferSizes)) {
                 mLinkProperties.setTcpBufferSizes(mTcpBufferSizes);
             }
+            mLinkProperties.setTcpDelayedAckSegments(mTcpDelayedAckSegments);
+            mLinkProperties.setTcpUserCfg(mTcpUserCfg);
+
             mNetworkAgent = new WifiNetworkAgent(getHandler().getLooper(), mContext,
                     "WifiNetworkAgent", mNetworkInfo, mNetworkCapabilitiesFilter,
                     mLinkProperties, DEFAULT_SCORE);
